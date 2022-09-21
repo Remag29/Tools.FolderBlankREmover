52ac2dd79260c96c613e251f22749b34ab83b6e0
Remag29 <tristan.lucas.29@orange.fr>
2022-09-21 16:00:11 +0200

    Add test to define if the file needs to be renamed

--- Private/Invoke-BlankRemove.ps1
+++ Private/Invoke-BlankRemove.ps1
@@ -7,7 +7,12 @@ function Invoke-BlankRemove {
     # get file name
     $file = Get-Item -Path $filePath
     $oldName = $file.BaseName
+    $newName = $($oldName.replace(' ', '_'))
 
-    # rename file with replacing ' ' by '_'
-    Rename-Item -Path $filePath -NewName $($oldName.replace(' ','_'))
+    if ($oldName -ne $newName) {
+        # rename file with replacing ' ' by '_'
+        Rename-Item -Path $filePath -NewName $($oldName.replace(' ', '_'))
+        Write-Verbose "[Invoke-BlankRemove] - ""$oldName"" was renamed"
+
+    }
 }
