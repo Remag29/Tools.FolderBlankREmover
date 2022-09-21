57209b124375c9e55f0830866372f52758c618bd
Remag29 <tristan.lucas.29@orange.fr>
2022-09-21 15:32:10 +0200

    Add function who diffine if item is a file or folder Call Invoke-RemoveBlank if file Call him self if folder

---
+++ Private/Invoke-BlankRemoveOnFolder.ps1
@@ -0,0 +1,25 @@
+function Invoke-BlankRemoveOnFolder {
+    param (
+        [Parameter(Mandatory = $true, Position = 1)] [string] $FolderPath
+    )
+
+    $folderContent = Get-ChildItem -Path $FolderPath
+
+    foreach ($item in $folderContent) {
+        if ($item.Mode -eq "d-----") {
+            # Its a folder
+            Invoke-BlankRemoveOnFolder -FolderPath $item.FullName
+        }
+        elseif ($item.Mode -eq "-a----") {
+            # Its a file
+            Invoke-BlankRemove -filePath $item.FullName
+        }
+        else {
+            # Format not suported yet
+        }
+    }
+
+    
+    $oldFolderName = $(Get-Item -Path $FolderPath).BaseName
+    Rename-Item -Path $FolderPath -NewName $oldFolderName.replace(' ', '_')
+}
